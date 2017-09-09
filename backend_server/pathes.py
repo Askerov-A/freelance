f492ab33b571115c258c093e667e1b7c1dd96c69
Azat <askazat97@gmail.com>
2017-09-09 10:15:59 +0600

    Deleting all

--- backend_server/pathes.py
+++
@@ -1,6 +0,0 @@
-import sys
-print('Hello')
-sys.path.append(sys.path[0]+'/app_server/models')
-sys.path.append(sys.path[0]+'/app_server/controllers')
-sys.path.append(sys.path[0]+'/app_server/routes')
-sys.path.append(sys.path[0]+'/app_server/views')
